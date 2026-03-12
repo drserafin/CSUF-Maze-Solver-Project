ce7748c4f6865b758906ad622a9e6ab16c90c2cb
Erik Argueta <argueta.erik@csu.fullerton.edu>
2026-03-12 10:10:03 -0700

    Started BFS

---
+++ bfs_solver.py
@@ -0,0 +1,28 @@
+"""
+BFS Solver.py
+Author: Erik Argueta
+Description: 
+    1. Finds shortest path (in number of steps) when a path exists 
+    2. Provides correctness testing and edge cases
+"""
+
+"""
+INPUT:
+    Your maze is a 2D grid of cells:
+        S = Start (exactly 1)
+        E = Exit/End (exactly 1)
+        # = Wall (blocked)
+        . = Open path (walkable)
+
+    Valid Moves:
+        - 4-direction movement only: Up, Down, Left, Right
+        - No diagonal moves
+    
+OUTPUT: 
+    1. Algortihm used (BFS vs DFS)
+    2. Path length (number of steps from S to E)
+    3. Number of visited/explored cells (how many nodes expanded)
+    4. Runtime (simple timing using Python time module)
+"""
+
+
