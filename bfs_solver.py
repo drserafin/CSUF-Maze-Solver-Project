0570019be5a6fb732ce66960ea16c5bbc3829aae
Erik Argueta <argueta.erik@csu.fullerton.edu>
2026-03-14 14:27:51 -0700

    finished fine-tuning and covered edge cases

--- bfs_solver.py
+++ bfs_solver.py
@@ -12,14 +12,6 @@ INPUT:
         # = Wall (blocked)
         . = Open path (walkable)
 
-        ##########
-        #S..#....#
-        #.#.##.#.#
-        #.#....#.#
-        #.####.#E#
-        #........#
-        ##########
-
     Valid Moves:
         - 4-direction movement only: Up, Down, Left, Right
         - No diagonal moves
@@ -29,20 +21,15 @@ OUTPUT:
     2. Path length (number of steps from S to E)
     3. Number of visited/explored cells (how many nodes expanded)
     4. Runtime (simple timing using Python time module)
-
-    
-
-    #######
-    #S...E#
-    #######
-
 """
 
 from collections import deque
+import time
+
 
 def intro(maze):
     print("Welcome to the Breadth First Solver (BFS) program.")
-    # print("Here is the maze being read:")
+    print("\nHere is the maze being read:")
     readFile(maze)
 
 
@@ -51,6 +38,7 @@ def readFile(maze):
         with open('mazes/basic_maze.txt', 'r') as file:
             for line in file:
                 clean_line = line.replace('\n', '')
+                print(clean_line)
                 maze.append(list(clean_line))
             # print(maze)
     except FileNotFoundError:
@@ -62,26 +50,27 @@ def findStart(maze):
     for row in range(len(maze)):
         for col in range(len(maze[row])):
             if maze[row][col] == 'S':
-                print(f"Start located at location: ({row}, {col})")
+                # print(f"\nStart located at location: ({row}, {col})")
                 return (row, col)
 
 # start: (row, col)
-def bfs(maze, start, success):
+def bfs(maze, start):
     visited = set()
     queue = [start]
     solution = [start]
     visited.add(start)
-    print(f"Running BFS Algorithm...")
+    # print(f"Running BFS Algorithm...")
 
+    start = time.time()
     while queue:
         vertex_row, vertex_col = queue.pop(0)
-        print(f"Current Vertex Coordinate: {vertex_row}, {vertex_col}")
+        # print(f"Current Vertex Coordinate: {vertex_row}, {vertex_col}")
 
         for move_row, move_col in neighbors:
             neighbor_row = vertex_row + move_row
             neighbor_col = vertex_col + move_col
 
-            if (neighbor_row, neighbor_col) not in visited:     # If the cell has NOT been visited
+            if neighbor_row >=0 and neighbor_col >= 0 and (neighbor_row, neighbor_col) not in visited:     # If the cell is within bounds AND has NOT been visited
                 visited.add((neighbor_row, neighbor_col))       # Add the cell to visited[]
 
                 if maze[neighbor_row][neighbor_col] == '.':     # If the cell being visited is '.'
@@ -89,16 +78,14 @@ def bfs(maze, start, success):
                     solution.append((neighbor_row, neighbor_col))   # add to the solution set
                 
                 elif maze[neighbor_row][neighbor_col] == 'E':    # If the cell being visited is the End
-                    print(f"Located End...")
+                    # print(f"Located End...")
                     solution.append((neighbor_row, neighbor_col))   # add to the solution set
-                    success = True
                     print(f"")
-                    return solution, len(visited)
-        
-        if success==False:
-            print(f"Solution not found...")
+                    end = time.time()
+                    return solution, len(visited), True, (end-start)
 
-    return solution, len(visited)
+    end = time.time()
+    return solution, len(visited), False, (end-start)
 
 neighbors = [
     (-1, 0),    # check row above
@@ -107,18 +94,21 @@ neighbors = [
     (0,1)       # check col right
 ]
 
-def printResults(solution, visited, success):
-    print(f"\nAlgortihm Used: BFS")
-    if success:
+def printResults(solution, visited, success, time_elapsed):
+    if success == True: print("Solution was found!") 
+    else: print("No Solution Found")
+    
+    print(f"Algortihm Used: BFS")
+    if success == True:
         print(f"The Length of the Shortest Path is: {len(solution)}")
-    if not success:
+    if success == False:
         print(f"No Solution Found.")
     
     print(f"Number of Visited Cells: {visited}")
+    print(f"Time elapsed: {time_elapsed: .8f}s")
 
 maze = []
-success = False
 intro(maze)
 start = findStart(maze)
-shortest_path, total_visited = bfs(maze, start, success)
-printResults(shortest_path, total_visited, success)
+shortest_path, total_visited, success, time_elapsed = bfs(maze, start)
+printResults(shortest_path, total_visited, success, time_elapsed)
