494b847bf12fa7fbd6a128330bc30ac227bd0621
Erik Argueta <argueta.erik@csu.fullerton.edu>
2026-03-14 13:56:30 -0700

    added Start and End conditional logic; added No Solution

--- bfs_solver.py
+++ bfs_solver.py
@@ -42,23 +42,17 @@ from collections import deque
 
 def intro(maze):
     print("Welcome to the Breadth First Solver (BFS) program.")
-    print("Here is the maze being read:")
+    # print("Here is the maze being read:")
     readFile(maze)
 
 
-def outro():
-    print("Algorithm Used: BFS")
-    print("Path Length: ")
-    print("Number of Visited Cells: ")
-    print("Runtime: ")
-
 def readFile(maze):
     try:
-        with open('mazes/easy_maze.txt', 'r') as file:
+        with open('mazes/basic_maze.txt', 'r') as file:
             for line in file:
                 clean_line = line.replace('\n', '')
                 maze.append(list(clean_line))
-            print(maze)
+            # print(maze)
     except FileNotFoundError:
         print("Error: File 'maze.txt' was not found.")
 
@@ -68,14 +62,16 @@ def findStart(maze):
     for row in range(len(maze)):
         for col in range(len(maze[row])):
             if maze[row][col] == 'S':
-                print(f"Start located at maze[{row}, {col}]")
+                print(f"Start located at location: ({row}, {col})")
                 return (row, col)
 
 # start: (row, col)
-def bfs(maze, start):
+def bfs(maze, start, success):
     visited = set()
     queue = [start]
+    solution = [start]
     visited.add(start)
+    print(f"Running BFS Algorithm...")
 
     while queue:
         vertex_row, vertex_col = queue.pop(0)
@@ -85,20 +81,44 @@ def bfs(maze, start):
             neighbor_row = vertex_row + move_row
             neighbor_col = vertex_col + move_col
 
-            if (neighbor_row, neighbor_col) not in visited:
-                visited.add((neighbor_row, neighbor_col))
+            if (neighbor_row, neighbor_col) not in visited:     # If the cell has NOT been visited
+                visited.add((neighbor_row, neighbor_col))       # Add the cell to visited[]
 
-                if maze[neighbor_row][neighbor_col] == '.':
-                    queue.append((neighbor_row, neighbor_col))
+                if maze[neighbor_row][neighbor_col] == '.':     # If the cell being visited is '.'
+                    queue.append((neighbor_row, neighbor_col))  # add this cell to queue to progress
+                    solution.append((neighbor_row, neighbor_col))   # add to the solution set
+                
+                elif maze[neighbor_row][neighbor_col] == 'E':    # If the cell being visited is the End
+                    print(f"Located End...")
+                    solution.append((neighbor_row, neighbor_col))   # add to the solution set
+                    success = True
+                    print(f"")
+                    return solution, len(visited)
+        
+        if success==False:
+            print(f"Solution not found...")
+
+    return solution, len(visited)
 
 neighbors = [
     (-1, 0),    # check row above
     (1, 0),     # check row below
     (0,-1),     # check col left
     (0,1)       # check col right
-]      
+]
+
+def printResults(solution, visited, success):
+    print(f"\nAlgortihm Used: BFS")
+    if success:
+        print(f"The Length of the Shortest Path is: {len(solution)}")
+    if not success:
+        print(f"No Solution Found.")
+    
+    print(f"Number of Visited Cells: {visited}")
 
 maze = []
+success = False
 intro(maze)
 start = findStart(maze)
-bfs(maze, start)
+shortest_path, total_visited = bfs(maze, start, success)
+printResults(shortest_path, total_visited, success)
