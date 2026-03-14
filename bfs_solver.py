5aed30747b899433d33ae74361caa6b2a9d1eeba
Erik Argueta <argueta.erik@csu.fullerton.edu>
2026-03-13 19:44:32 -0700

    created findStart and working on bfs function

--- bfs_solver.py
+++ bfs_solver.py
@@ -40,10 +40,10 @@ OUTPUT:
 
 from collections import deque
 
-def intro():
+def intro(maze):
     print("Welcome to the Breadth First Solver (BFS) program.")
     print("Here is the maze being read:")
-    readFile()
+    readFile(maze)
 
 
 def outro():
@@ -52,20 +52,43 @@ def outro():
     print("Number of Visited Cells: ")
     print("Runtime: ")
 
-def readFile():
+def readFile(maze):
     try:
         with open('mazes/basic_maze.txt', 'r') as file:
-            maze = file.read()
+            for line in file:
+                clean_line = line.replace('\n', '')
+                maze.append(list(clean_line))
             print(maze)
     except FileNotFoundError:
         print("Error: File 'maze.txt' was not found.")
 
 
-def setMaze():
-    rowSize = int(input("Enter the total rows in the maze: "))
-    colSize = int(input("Enter the total columns in the maze: "))
-    V = rowSize * colSize
+# finds the location index of where the maze Starts
+def findStart(maze, start):
+    for row in range(len(maze)):
+        for col in range(len(maze[row])):
+            if maze[row][col] == 'S':
+                print(f"Start located at maze[{row}, {col}]")
+                return (row, col)
 
+def bfs(maze, start):
+    visited = set()
+    queue = [start]
+    visited.add(start)
 
+    while queue:
+        vertex = queue.pop(0)
+        print(f"Current Vertex: {vertex}")
 
-intro()
+        for neighbor in maze[vertex]:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append(neighbor)
+
+              
+
+start = None
+maze = []
+intro(maze)
+findStart(maze, start)
+
